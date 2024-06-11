91175be1c11e623f8238b54d548fc343e7b30e08
MichaelKiuna <141023286+MichaelKiuna@users.noreply.github.com>
2024-06-11 20:21:02 +0300

    Add files via upload

---
+++ NewFile1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Strathmore University Vision\n";
+    std::cout << "To become a leading out- come driven entrepreneurial research\n";
+    std::cout << "University by translating our excellence into major contribution\n";
+    std::cout << "to culture, economic well-being and quality of life.\n";
+    return 0;
+}
---
+++ NewFile3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "        O       O\n";    // Heads of the stick figures
+    std::cout << "       /|\\     /|\\\n";  // Arms and bodies (holding hands)
+    std::cout << "       / \\     / \\\n";  // Legs
+    std::cout << "        |       |\n";   // Separation line to indicate hands are held
+    std::cout << "    _________\n";      // Top of the car
+    std::cout << "   /         \\\n";      // Upper part of the car
+    std::cout << "  /___________\\\n";    // Bottom part of the car
+    std::cout << "   o       o\n";        // Wheels of the car
+    std::cout << "=================";   // Ground
+
+    return 0;
+}
